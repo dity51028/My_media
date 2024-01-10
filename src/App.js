79b0a3cc0a55feb4cd163ad7c6c6203edcc7b58c
Dipannita_Mondal <dipannitamisti10@gmail.com>
2024-01-10 21:08:11 +0530

    sidebar implemented

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import NavBar from './components/NavBar';
+import Feed from './components/Feed';
+import VideoDetail from './components/VideoDetail';
+import ChannelDetail from './components/ChannelDetail';
+import SearchFeed from './components/SearchFeed';
+import Sidebar from './components/Sidebar';
+
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Box sx={{backgroundColor:'#000'}}>
+        <NavBar/>
+        <Routes>
+            <Route path='/' exact element={<Feed/>} />
+            <Route path='/video/:id' element={<VideoDetail/>}/>
+            <Route path='/channel/:id' element={<ChannelDetail/>}/>
+            <Route path='/search/:searchTerm' element={<SearchFeed/>}/>
+        </Routes>
+
+    </Box>
+        
+    </BrowserRouter>
+  )
+}
+
+export default App
