fb40029a5cc959640fd82b01ee8cfde25b64b55c
Dipannita_Mondal <dipannitamisti10@gmail.com>
2024-01-23 21:45:03 +0530

    ChannelCard & VideoCard Added

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import { Box,CardContent,CardMedia,Typography,typography } from "@mui/material"
+import { CheckCircle } from "@mui/icons-material"
+import { Link } from "react-router-dom"
+import { demoProfilePicture } from "../utils/constants"
 
-const ChannelCard = () => {
+
+const ChannelCard = ({channelDetail}) => {
   return (
-    <div>ChannelCard</div>
+   <Box
+    sx={{
+      boxShadow:'none',
+      borderRadius:'20px',
+      display:'flex',
+      justifyContent:'center',
+      width:{xs:'356px',md:'320px'},
+      height :'326px',
+      margin:'auto'
+
+    }}
+   >
+    <Link to={`/channel/${channelDetail?.channelId}`}>
+      <CardContent
+        sx={{
+          display:'flex',flexDirection:'column',justifyContent:'center',textAlign:'center',color:'#fff'
+        }}
+      >
+
+        <CardMedia
+          image={channelDetail?.thumbnails?.high?.url || demoProfilePicture}
+          alt={channelDetail?.title}
+          sx={{borderRadius:'50%',height:'180px',width:'180px',mb:2,border:'1px solid #e3e3e3'}}
+        />
+        <Typography variant="h6">
+           {channelDetail?.title}
+           <CheckCircle sx={{fontSize:14, color:'gray',ml:'5'}}/>
+        </Typography>
+        {channelDetail?.statistics?.subscribersCount && (
+          <Typography>
+            {parseInt(channelDetail?.satistics?.subscribersCount).toLocaleString()}
+              Subscribers
+          </Typography>
+        )}
+        
+        
+      </CardContent>
+    
+    </Link>
+
+   </Box>
   )
 }
 
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -5,10 +5,42 @@ import { CheckCircle } from '@mui/icons-material'
 import { demoThumbnailUrl,demoVideoUrl,demoChannelUrl,demoVideoTitle,demoChannelTitle } from '../utils/constants'
 
 
-const VideoCard = ({video}) => {
+const VideoCard = ({video:{videoId,thumbnails}} ) => {
+  console.log(videoId,thumbnails)
   return (
-    <div>VideoCard</div>
+   <Card sx={{width:{md:'320px',xs:'100%'},boxShadow:'none',borderRadius:0}}>
+
+    <Link to={videoId?`/video/${videoId}`:demoVideoUrl}>
+    <CardMedia 
+      image={thumbnails?.high?.url}
+      alt={thumbnails?.title}
+      sx={{height:358,height:180}}
+    />
+    </Link>
+
+
+    <CardContent sx={{backgroundColor:'#1e1e1e',height:'106px'}}>
+      <Link to={videoId?`/video/${videoId}`:demoVideoUrl}>
+        <Typography variant='subtitle1' fontWeight='bold' color='#FFF'>
+          {thumbnails?.title.slice(0,60)||demoVideoTitle.slice(0,60)}
+        </Typography>
+      </Link>
+   
+    </CardContent>
+
+   </Card>
+  
   )
 }
 
 export default VideoCard
+
+
+ /** 
+      <Link to={channelId?`/video/${channelId}`:demoChannelUrl}>
+        <Typography variant='subtitle2' fontWeight='bold' color='gray'>
+          {thumbnails?.title.slice(0,60)||demoChannelTitle.slice(0,60)}
+          <CheckCircle sx={{fontSize:12, color:'gray',ml:'5'}}/>
+        </Typography>
+      </Link>
+      */
